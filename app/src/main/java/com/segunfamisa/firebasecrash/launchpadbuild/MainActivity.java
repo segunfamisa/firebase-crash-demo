b7e9e1848ffca2642fac96fc5b34ee0c1496ff11
Segun Famisa <segunfamisa@gmail.com>
2016-09-21 23:55:48 +0100

    chore: Added code to report crash and log events

--- app/src/main/java/com/segunfamisa/firebasecrash/launchpadbuild/MainActivity.java
+++ app/src/main/java/com/segunfamisa/firebasecrash/launchpadbuild/MainActivity.java
@@ -2,9 +2,12 @@ package com.segunfamisa.firebasecrash.launchpadbuild;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+
+import com.google.firebase.crash.FirebaseCrash;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
         mCrashButton = (Button) findViewById(R.id.button_crash);
         mCheckCatchError = (CheckBox) findViewById(R.id.checkbox_catch_error);
 
-
+        // set button click listener
         mCrashButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // send log event
+                FirebaseCrash.log("User clicked crash button");
+
                 // if catch error checkbox is checked, do a try-catch
                 if (mCheckCatchError.isChecked()) {
                     try {
@@ -39,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // send log event (but use logcat this time)
+        FirebaseCrash.logcat(Log.DEBUG, "TAG", "User resumed main activity");
     }
 
     /**
